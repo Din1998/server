6be06afded812db9185bfe686d97693b7ca74d1b
din1998 <dinislam8803@gmail.com>
2023-02-20 17:25:04 +0600

    change api

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json())
 app.use(express.json())
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://bd-blog.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true
   })
