e9a076ea6f863e9e84acd9c61b91259ab5604f23
din1998 <dinislam8803@gmail.com>
2023-03-05 21:27:47 +0600

    modify api url

--- route/authRoute.js
+++ route/authRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const passport  = require('passport');
 
 //
-const CLIENT_URL = "https://bd-blog-jqwn-27d6xebeh-din1998.vercel.app";
+const CLIENT_URL = "https://bd-blog.vercel.app";
 
 //
 router.get("/login/success", (req, res) => {
